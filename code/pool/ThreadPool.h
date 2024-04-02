abf0ba3d8458d91f819845e2459e0953c862405a
Dirck-YHP <1941803238@qq.com>
2024-04-02 19:43:08 +0800

    线程池添加注释

--- code/pool/ThreadPool.h
+++ code/pool/ThreadPool.h
@@ -19,7 +19,9 @@ public:
             threads_.emplace_back([this] {
                 while (true) {
                     std::unique_lock<std::mutex> lock(mtx_);
-                    condition_.wait(lock, [this] {
+
+                    // 等待，判断是否有新的任务需要执行，如果线程池已经关闭且任务队列为空，则退出线程
+                    condition_.wait(lock, [this] {              
                         return !tasks_.empty() || isClosed_;
                     });
 
@@ -29,7 +31,7 @@ public:
 
                     std::function<void()> task (std::move(tasks_.front()));
                     tasks_.pop();
-                    lock.unlock();
+                    lock.unlock();  // 任务已经取出来了，所以提前解锁，让其他线程可以继续添加任务
                     task();
                 }
             });
@@ -42,6 +44,7 @@ public:
             isClosed_ = true;
         }
 
+        // 通知所有等待的线程线程池已经关闭,join()等待所有线程结束。
         condition_.notify_all();
         for (auto& t : threads_){
             t.join();
@@ -50,14 +53,15 @@ public:
 
     template<class F, class... Args>
     void AddTask(F&& f, Args&&... args){
+        // 将函数f和其参数args...绑定到task上
         std::function<void()> task = std::bind(std::forward<F>(f), std::forward(args)...);
         
         {
             std::unique_lock<std::mutex> lock(mtx_);
-            tasks_.emplace(std::move(task));
+            tasks_.emplace(std::move(task));        // 添加到任务队列中
         }
 
-        condition_.notify_one();
+        condition_.notify_one();        // 通知等待的线程有新任务可以执行，唤醒一个程序
     }
 
 private:
