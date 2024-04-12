6e69c21d0e78fc24e4023391a38c25d23a8b356c
Dirck-YHP <1941803238@qq.com>
2024-04-12 08:41:34 +0800

    添加前段资源,已经能正常登入和登出

--- code/main.cpp
+++ code/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include "server/WebServer.h"
 
 int main()
 {
-    std::cout << "my webserver 20240409" << std::endl;
+    WebServer server(
+        9006, 3, 60000,     // 端口 ET模式 timeoutMs 
+        3306, "root", "yhp20001122..", "my_webserver_db",   //Mysql配置 
+        12, 8, true, 1, 1024);  // 连接池数量 线程池数量 日志开关 日志等级 日志异步队列容量
+    server.Start();
+
 
     return 0;
 }
